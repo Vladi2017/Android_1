b46cf730e80883bb1ae8abaa70175d88cc5202ea
Vladimir Manolescu <mvmanol@yahoo.com>
2020-12-30 16:19:39 +0200

    Nothing changed.., just some makeup..
    Untracked some files:
    git rm -r --cached projectFilesBackup/
    git rm -r --cached .idea
    git rm --cached *.iml
    
    .. and add the other files in utils1lib/, just to have a backup, possible to untrack utils1lib/ in the future..

---
+++ utils1lib/src/androidTest/java/ro/vladi/utils1lib/ChooseDialogTest.java
@@ -0,0 +1,17 @@
+package ro.vladi.utils1lib;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by batwings on 9/27/17.
+ */
+public class ChooseDialogTest {
+    @Test
+    public void get() throws Exception {
+        ChooseDialog.class
+    }
+
+}
+
